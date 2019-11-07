fe3a18c53c4aec5758c4c45e7fa6fbd48ac680c6
fang duan <df1228@gmail.com>
2019-11-07 18:16:13 +0800

    Remove unused pages

--- src/app.js
+++ src/app.js
@@ -8,15 +8,7 @@ class App extends Component {
   config = {
     pages: [
       'pages/index/index',
-      'pages/basic/index',
-      'pages/view/index',
-      'pages/view/article/index',
-      'pages/feedback/index',
-      'pages/data-entry/index',
       'pages/layout/index',
-      'pages/navigation/index',
-      'pages/navigation/indexes/index',
-      'pages/advanced/index',
       'pages/order/index',
       'pages/detail/index',
       'pages/bindPhone/index',
